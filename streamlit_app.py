01f87cec27c320c1a914ef822b9925dee7fd344d
phat-ap <phat.ap@outlook.com>
2023-01-26 23:08:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,28 +6,15 @@ import plotly.graph_objects as go
 import api_bank_of_thailand_0_2 as ab
 import api_moc as am
 
-df_of_categories = ab.get_category_df()
 
 
 
 
 add_sidebar = st.sidebar.selectbox('Select View', 
-                                   ('Test', 
-                                    'CPI',
+                                   ('CPI',
                                     'Series List'
                                     )
                                    )
-if add_sidebar == 'Test':
-    col1, col2 = st.columns(2)
-    EC_EI_020 = ab.BOTCategory('EC_EI_020')
-    bot_df_EC_EI_020 = EC_EI_020.get_obs_df()
-    with col1:
-        st.line_chart(pd.to_numeric(bot_df_EC_EI_020['EIEXUSDM00159']))
-    with col2:
-        st.line_chart(pd.to_numeric(bot_df_EC_EI_020['EIIMUSDM00161']))
-    st.line_chart(pd.to_numeric(bot_df_EC_EI_020['EIIMUSDM00160']))
-    fig = go.Figure(data = [go.Scatter(x = bot_df_EC_EI_020.index, y = pd.to_numeric(bot_df_EC_EI_020['EIIMUSDM00160']), mode = 'lines')], layout = {'title': 'EIIMUSDM00160'})
-    st.write(fig)
 if add_sidebar == 'CPI':
     
     fig = go.Figure(data = [go.Scatter(name='CPI',
