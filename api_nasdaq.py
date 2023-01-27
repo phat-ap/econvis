054a75cc6c9115a62e3ed34723ed81ca09ff98de
phat-ap <phat.ap@outlook.com>
2023-01-27 10:31:46 +0700

    Add Dubai from nasdaq

---
+++ api_nasdaq.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 08:41:28 2023
+
+@author: phata
+"""
+
+
+'qcrfuPqxUf4rEx4a7PCV'
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly
 import plotly.graph_objects as go
+import quandl
 
 import api_bank_of_thailand_0_2 as ab
 import api_moc as am
@@ -37,6 +38,12 @@ if add_sidebar == 'CPI':
     fig.update_layout(barmode='stack')
     fig.update_layout(legend=dict(orientation="h",yanchor="bottom",y=1.02,xanchor="right",x=1))
     st.plotly_chart(fig, use_container_width=True)
+    
+    
+    df_dubai = quandl.get("ODA/POILDUB_USD")
+    df_dubai.index = df_dubai.index.map(lambda dtindex: dtindex.replace(day = 1))
+    df_dubai/df_dubai.shift(12)*100-100
+    
     
     
 if add_sidebar == 'Series List':
