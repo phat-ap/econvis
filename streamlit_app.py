9556102a0e1d587897895eed466cd41f3389073c
phat-ap <phat.ap@outlook.com>
2023-01-27 08:41:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,6 @@ if add_sidebar == 'CPI':
     fig.update_layout(legend=dict(orientation="h",yanchor="bottom",y=1.02,xanchor="right",x=1))
     st.plotly_chart(fig, use_container_width=True)
     
-    st.write(st.secrets["nasdaq"]["api_key"])
     
 if add_sidebar == 'Series List':
     pass
