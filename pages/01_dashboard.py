5d37b205f86f5cc66ccd5d365d1e155607ef1932
phat-ap <phat.ap@outlook.com>
2023-02-08 15:36:23 +0700

    Update 01_dashboard.py

--- pages/01_dashboard.py
+++ pages/01_dashboard.py
@@ -32,7 +32,37 @@ def fetch_and_clean_data():
                        )
                .pct_change(12)
                ) * 100
-    df = df_disb
+    # Agriculture
+    df_agri = (pd
+               .read_csv('https://raw.githubusercontent.com/phat-ap/econvis/main/data/tha_agri_key_indicators.csv', 
+                         index_col = 0, 
+                         parse_dates = True)
+               .rename(columns = {'agri_prod_index': 'Agricultural Production', 
+                                  'agri_price_index': 'Agricultural Price'
+                                  }
+                       )
+               )
+    df_agri['Agricultural Income'] = df_agri['Agricultural Production'] * df_agri['Agricultural Price']
+    df_agri = df_agri.pct_change(12) * 100
+    
+    # Manufacturing
+    df_manu = (pd
+               .read_csv('https://raw.githubusercontent.com/phat-ap/econvis/main/data/tha_manu_key_indicators.csv', 
+                         index_col = 0, 
+                         parse_dates = True)
+               .drop(columns = ['capu_sa', 'mpi_sa'], axis = 1)
+               .rename(columns = {'mpi': 'Manufacturing Production', 
+                                  'capu': 'Capacity Utilization'
+                                  }
+                       )
+               )
+    df_manu['Manufacturing Production'] = df_manu['Manufacturing Production'].pct_change(12) * 100
+    df_manu = df_manu[['Manufacturing Production', 'Capacity Utilization']]
+    
+    
+    
+    # Merge
+    df = pd.concat([df_disb, df_agri, df_manu], axis = 1)
     # Format date
     df.index = df.index.format(formatter = lambda t: t.strftime("%b %y"))
     return df
@@ -47,10 +77,19 @@ def gmap_column_q(gmap, column, q = .20):
     gmap[column] = gmap[column].clip(upper = 1., lower = -1.)
     return gmap
 
+
+
 # gmap
 gmap = df.copy()
 gmap = gmap_column_q(gmap, 'Current Expenditure')
 gmap = gmap_column_q(gmap, 'Capital Expenditure')
+gmap = gmap_column_q(gmap, 'Agricultural Production')
+gmap = gmap_column_q(gmap, 'Agricultural Price')
+gmap = gmap_column_q(gmap, 'Agricultural Income')
+gmap = gmap_column_q(gmap, 'Manufacturing Production')
+gmap = gmap_column_q(gmap, 'Capacity Utilization', q = .001)
+
+
 
 
 # sidebar
@@ -67,7 +106,7 @@ st.markdown(
 """
 )
 col1, col2 = st.columns([1, 3])
-n_months = col1.number_input('Number of months', min_value=1, max_value=36, value=6)
+n_months = col1.number_input('Number of months', min_value=1, value=6)
 
 st.dataframe(data = df.tail(n_months)
              .applymap('{:.1f}'.format)
